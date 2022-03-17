3e6ffb454c9e4a3a07617095d70e1577ff1a9412
sukantg-007 <72221521+sukantg-007@users.noreply.github.com>
2022-03-17 19:12:00 +0530

    Add files via upload

--- StudentUtilModule.py
+++ StudentUtilModule.py
@@ -1,10 +1,8 @@
 import mysql.connector
 from studentinfo import Student
-
 class StudentUtil:
     conn = mysql.connector.connect(host = "localhost",user ="root", password = "root",database = "collegedb")        
     cursor = conn.cursor()
-
     @staticmethod
     def getallstd():
         try:        
@@ -14,7 +12,6 @@ class StudentUtil:
         except mysql.connector.Error as msg:
             print(msg)    
         return t
-
     @staticmethod
     def getstdbyid(id):
         try:        
@@ -24,34 +21,76 @@ class StudentUtil:
             return t if t else "Record not found"
         except mysql.connector.Error as msg:                        
             return msg
-
     @staticmethod
-    def stdupdate(id):
+    def stdupdate(id,**kwargs):  
         try:
             query = "select * from studenttb where id = {}".format(id)
             StudentUtil.cursor.execute(query)
-            t = StudentUtil.cursor.fetchone()
+            t = StudentUtil.cursor.fetchone()            
             if t:
-                new_name = input("Enter name to update :")
-                query = "update studenttb set sname='{}' where id={}".format(new_name,id)
+                if 'name' in kwargs and 'addr' not in kwargs :                    
+                    query = "update studenttb set sname='{}' where id={}".format(kwargs["name"],id)
+                elif 'addr' in kwargs and 'sclass' not in kwargs:
+                    query = "update studenttb set saddr='{}' where id={}".format(kwargs["addr"],id)
+                elif 'sclass' in kwargs and 'name' not in kwargs:
+                    query = "update studenttb set sclass='{}' where id={}".format(kwargs["sclass"],id)
+                else:
+                    query = "update studenttb set sname = '{}' ,saddr = '{}' ,sclass='{}' where id={}".format(kwargs["name"],kwargs["addr"],kwargs["sclass"],id)
                 StudentUtil.cursor.execute(query)
-                return "Successfully Updated"
+                StudentUtil.conn.commit()
+                return "Succefully updated"
             else:
-                return "No record found"
+                return "Record not found"   
         except mysql.connector.Error as msg:
             print(msg)
-
     @staticmethod
     def Savestudent(std):
         try:
-            query = "INSERT INTO STUDENTTB VALUES ({},'{}','{}','{}')".format(std.id,std.sname,std.saddr,std.sclass)
+            query = "INSERT INTO STUDENTTB(sname,saddr,sclass) VALUES ('{}','{}','{}')".format(std.sname,std.saddr,std.sclass)
             StudentUtil.cursor.execute(query)
             StudentUtil.conn.commit()
             return "Succeessfully inserted"
         except mysql.connector.Error as msg:
             return msg 
-    
     @staticmethod
     def deletestd(id):
-        #delete student
-        pass
+        try:
+            query = "DELETE FROM STUDENTTB WHERE ID = {}".format(id)
+            StudentUtil.cursor.execute(query)
+            StudentUtil.conn.commit()
+            return "Succeessfully deleted"
+        except mysql.connector.Error as msg:
+            return msg 
+    @staticmethod
+    def showallstd():
+        try:
+            query = "SELECT * FROM STUDENTTB "
+            StudentUtil.cursor.execute(query)
+            m = StudentUtil.cursor.fetchall()
+            print("{:<5}{:10}{:10}{:10}".format("ID","NAME","ADDR","CLASS"))
+            for t in m:
+                print("{:<5}{:10}{:10}{:10}".format(t[0],t[1],t[2],t[3]))
+                #print('',t[0],'\t',t[1],'\t',t[2],'\t',t[3])
+            StudentUtil.conn.commit()
+            return "Succeessfully Shown All Student"
+        except mysql.connector.Error as msg:
+            return msg 
+    @staticmethod
+    def searchstdby(**kwargs):
+        try:
+            if 'sid' in kwargs:
+                query = "SELECT * FROM STUDENTTB WHERE ID LIKE '{}%'".format(kwargs["sid"])
+            elif 'sname' in kwargs:
+                query = "SELECT * FROM STUDENTTB WHERE SNAME LIKE '{}%'".format(kwargs["sname"])
+            elif 'saddr' in kwargs:
+                query = "SELECT * FROM STUDENTTB WHERE SADDR LIKE '{}%'".format(kwargs["saddr"])
+            elif 'sclass' in kwargs:
+                query = "SELECT * FROM STUDENTTB WHERE SCLASS LIKE '{}%'".format(kwargs["sclass"])
+            StudentUtil.cursor.execute(query)
+            m = StudentUtil.cursor.fetchall()
+            print(" ID\t NAME\t ADDR\t CLASS\t")
+            for t in m:
+                print('',t[0],'\t',t[1],'\t',t[2],'\t',t[3])
+            StudentUtil.conn.commit()
+        except mysql.connector.Error as msg:
+            return msg 
--- studentinfo.py
+++ studentinfo.py
@@ -1,8 +1,7 @@
 class Student:
-    def __init__(self,id,sname,saddr,sclass):
-        self.id = id
+    def __init__(self,sname,saddr,sclass):
         self.sname = sname
         self.saddr = saddr
         self.sclass = sclass
     def __str__(self):
-        return "Id : {}\tName :{}\tAddr :{}\tClass :{}".format(self.id,self.sname,self.saddr,self.sclass)
+        return "Name :{}\tAddr :{}\tClass :{}".format(self.sname,self.saddr,self.sclass)
